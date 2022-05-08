ad2ba9bbd24a480ddb451338d3ba1bb052fea667
Linus Ingvarsson <liinus.ingvarsson@gmail.com>
2022-05-08 23:32:07 +0200

    Rättade till felen i produktionskoden

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ window.onload = function () {
 
     push.addEventListener("click", function () {
         var x = prompt("Vad ska vi lägga på stacken?");
-        stack.push();
+        stack.push(x);
         display.innerHTML = x;
     });
 
